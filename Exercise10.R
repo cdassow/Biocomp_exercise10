ceab803cc9aed6c638cc54a6a2edb27f031f1784
Grace Petrosini <gpetrosi@nd.edu>
2019-12-06 10:54:33 -0500

    tweaking variables and loop to fit problem

--- Exercise10.R
+++ Exercise10.R
@@ -9,13 +9,12 @@ library(ggplot2)
 N0=3
 M0=5
 # parameters: growth rate and limitations
-rN=0.1
-rM=0.1
-K=100
+rN=rM=0.1
+K=1000000
 timesteps = 100
 treatment = T
 
-# create vector to store N's
+# create vector to store N's and M's
 Ns=numeric(length=timesteps)
 Ns[1]=N0
 Ms=numeric(length=timesteps)
@@ -24,12 +23,24 @@ Ms[1]=M0
 #for loop
 for(t in 1:timesteps){
   if (treatment = T){
-    
+    #normal population
+    N[t+1] = N[t]+rN*N[t](1-(N[t]+M[t]/K))
+    #mutant population
+    M[t+1] = M[t]+rM*M[t](1-(N[t]+M[t]/K))
+  }else if (treatment = F){
+    rM=rN*.5
+    rN=-0.1
+    #normal population
+    N[t+1] = N[t]+rN*N[t](1-(N[t]+M[t]/K))
+    #mutant population
+    M[t+1] = M[t]+rM*M[t](1-(N[t]+M[t]/K))
   }
 }
 
-#normal population
-N[t+1] = N[t]+rN*N[t](1-(N[t]+M[t]/K))
 
-#mutant population
-M[t+1] = M[t]+rM*M[t](1-(N[t]+M[t]/K))
+# plot simulation
+library(ggplot2)
+popN<-data.frame(time=1:length(Ns),N=Ns)
+popN<-data.frame(time=1:length(Ms), M=Ms)
+ggplot(data=pop, aes(x=time, y=N))+geom_line()
+ggplot(data=pop, aes(x=time, y=M))+geom_line()
