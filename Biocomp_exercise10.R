128c883639eb171744be3f23d5f45b5ab2639365
Sydney Louden <slouden@nd.edu>
2019-12-10 11:24:24 -0500

    updated function

--- Biocomp_exercise10.R
+++ Biocomp_exercise10.R
@@ -1,44 +1,62 @@
-#Mutation doesn't affect growth rate when cancer drug is absent
-#When drug is present, mutant population grows at 50% of regular growth rate
-#When drug is present, regular population declines
-
-#Regular population equation
-#Nt+1=Nt + rN*Nt*(1-(Nt+Mt)K)
-
-#Mutant Population equation
-#Mt+1=Mt + rM*Mt*(1-(Nt+Mt)/K)
-
-#Under normal conditions:
-  #rN=rM=0.1
-  #K=1,000,000
-
-#Mutation occurred at Nt=100
-#rN = -0.1
-
 #Function to model the growth of the two populations
 #Ms is the population vector of the mutatant cells
 #Ns is the population vector of the normal cells
 #K is the carrying capacity
 #rN is the growth rate of the normal cells, and rM is the growth rate of the mutant cells
+#rN2 is the growth rate of the normal cells once the drug takes affect, and rM2 is the growth rate of the mutant cells once the drug takes affect
 #timesteps is the number of cell divions 
-#At a population size of 100 cells, the drug takes affect 
-TumorPop<-function(N0=1, M0=1, K=1000000, timesteps=250, rN=0.1, rM=0.1){
+#Drug takes affect at t=100
+#Determine when N[t]=100
+  time=100
+  N0=1
+  K=1000000
+  rN=0.1
+    for(t in 1:(timesteps-1)){
+      Ns[t+1]<-Ns[t]+(rN*Ns[t]*(1-(Ns[t]/K)))
+    }
+  Ns
+  Ns[49]
+  Ns[50]
+  #Ns reaches 100 between 49 and 50, so the mutation will occur at t=49
+TumorPop<-function(No=1, Mo=1, timesteps=750, K=1000000, rN=0.1, rM=0.1, rN2=-0.1, rM2=0.05){
   Ns<-numeric(timesteps)
+  Ns[1]<-No
   Ms<-numeric(timesteps)
-  Ns[1]<-N0
-  Ms[1]<-M0
+  Ms[49]<-Mo
+  rN2=-0.1
+  rM2=0.05
   for(t in 1:(timesteps-1)){
-    if(Ns[t]<100){
-      Ns[t+1]<-Ns[t]+rN*Ns[t]*(1-((Ns[t]+Ms[t])/K))
-      Ms[t+1]<-Ms[t]+rM*Ms[t]*(1-((Ms[t]+Ns[t])/K))
+    if(t<49){
+      rN=rN
+      Ns[t+1]<-Ns[t]+(rN*Ns[t]*(1-(Ns[t]/K)))
     }else{
-      rN=-rN
-      rM=0.5*rM
-      Ns[t+1]<-Ns[t]+rN*Ns[t]*(1-((Ns[t]+Ms[t])/K))
-      Ms[t+1]<-Ms[t]+rM*Ms[t]*(1-((Ms[t]+Ns[t])/K))
+      rN=rN
+      rM=rM
+      Ns[t+1]<-Ns[t]+(rN*Ns[t]*(1-((Ns[t]+Ms[t])/K)))
+      Ms[t+1]<-Ms[t]+(rM*Ms[t]*(1-((Ms[t]+Ns[t])/K)))
+    }
+    if(t>=100){
+      rN=rN2
+      rM=rM2
+      Ns[t+1]<-Ns[t]+(rN*Ns[t]*(1-((Ns[t]+Ms[t])/K)))
+      Ms[t+1]<-Ms[t]+(rM*Ms[t]*(1-((Ms[t]+Ns[t])/K)))
     }
   }
+  NormalPop<<-as.numeric(Ns)
+  MutantPop<<-as.numeric(Ms)
 }
-#Run the function
+
 TumorPop()
 
+
+Population<-data.frame(time=1:750)
+Population$NormalPopulation<-NormalPop
+Population$MutantPopulation<-MutantPop
+
+#Graph Data
+#Load ggplot
+library(ggplot2)
+#Create Plot
+ggplot(data=Population)+
+  geom_line(aes(x=time,y=NormalPopulation), col="blue")+
+  geom_line(aes(x=time,y=MutationPopulation), col="red")
