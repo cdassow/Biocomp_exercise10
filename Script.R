889aca49b981ca60435907b809c7c9be36db39b6
aphubert <ahubert@nd.edu>
2019-12-11 21:32:05 -0500

    Final

--- Script.R
+++ Script.R
@@ -2,35 +2,34 @@
 #sets up base parameters for this, can be edited for flexibility. 
 no_drug_rate <- 0.1
 K <- 1000000
-N0 <- 100
-M0 <- 100
 drug_rate_no_mutation <- -0.1
-timestep <- 250
+timestep <- 1000
 
 #must make location for the  
-Nt=numeric(length=timestep)
-Mt=numeric(length=timestep)
-  
+Nt <- numeric(length=timestep)
+Mt <- numeric(length=timestep)
+Nt[1] <- 99
+Mt[1] <- 1  
 #load ggplot 
 library(ggplot2)
 
-#set a value to change once the drug is applied
-drug <- F
-
 #begin for loop
-for(i in 100:timestep-1){
-  #if statement so the first growth only occurs before the system reaches carrying capacity
-  if(drug==F){
-    Nt=Nt+no_drug_rate*Nt(1-(Nt+Mt)/K)
-    Mt=Mt+no_drug_rate*Mt(1-(Nt+Mt)/K)
-      if((Nt+Mt)/K==1){
-        drug <- T
-      }
+for(i in 1:(timestep-1)){
+  
+  #if statement so the first growth only occurs before the system reaches equilibrium, can adjust when the drug is applied by changing 3. 
+  if(i<timestep/3){
+    Nt[i+1]=Nt[i]+no_drug_rate*Nt[i]*(1-(Nt[i]+Mt[i])/K)
+    Mt[i+1]=Mt[i]+no_drug_rate*Mt[i]*(1-(Nt[i]+Mt[i])/K)
   }
   else{
-    Nt=Nt+drug_rate_no_mutation*Nt(1-(Nt+Mt)/K)
-    Mt=Mt+no_drug_rate*0.5*Mt(1-(Nt+Mt)/K)
+    Nt[i+1]=Nt[i]+drug_rate_no_mutation*Nt[i]*(1-(Nt[i]+Mt[i])/K)
+    Mt[i+1]=Mt[i]+no_drug_rate*0.5*Mt[i]*(1-(Nt[i]+Mt[i])/K)
   }
-    return
 }
-  
+#create a dataframe and then plot of the results
+cancer <- data.frame(time=1:length(Nt),N_of_Nonmutants=Nt, N_of_Mutants=Mt)
+ggplot(data=cancer)+geom_line(mapping=aes(x=time, y=N_of_Nonmutants))+
+  geom_line(mapping=aes(x=time, y=N_of_Mutants))+
+  labs(y="Population Size", x="Time")+ggtitle("Mutant and Non-Mutant Population Sizes over Time")
+
+
